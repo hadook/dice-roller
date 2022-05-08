40f641b881292bc113e613559a5e2cb5e911f64f
hadook <hadook13@gmail.com>
2022-05-08 02:22:17 +0200

    Calculator class stub created

---
+++ calc.py
@@ -0,0 +1,30 @@
+from random import randint
+
+class Calculator:
+    """This class is responsible for performing integer calculations."""
+
+    def __init__(a, b, operator) -> None:
+        pass
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def substract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+    @staticmethod
+    def divide(a, b):
+        return a // b
+
+    @staticmethod
+    def roll(a, b):
+        rolls = []
+        for i in range(a):
+            rolls.append(randint(1, b))
+        return rolls
