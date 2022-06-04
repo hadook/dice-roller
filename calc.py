1454c20bc22672dada2b40b8bfc7d56a39d678ea
hadook <hadook13@gmail.com>
2022-06-04 02:30:20 +0200

    Added dice operations to Calculator class

--- calc.py
+++ calc.py
@@ -1,60 +1,64 @@
-from random import randint
+from roll import Roll
+
 
 class Calculator:
     """This class is responsible for performing integer calculations."""
 
-    def __init__() -> None:
-        pass
-
     @staticmethod
-    def add(a, b):
+    def add(a: int, b: int) -> int:
         return a + b
 
     @staticmethod
-    def substract(a, b):
+    def substract(a: int, b: int) -> int:
         return a - b
 
     @staticmethod
-    def multiply(a, b):
+    def multiply(a: int, b: int) -> int:
         return a * b
 
     @staticmethod
-    def divide(a, b):
+    def divide(a: int, b: int) -> int:
         return a // b
 
     @staticmethod
-    def roll(a, b):
-        rolls = []
-        for i in range(a):
-            rolls.append(randint(1, b))
-        return rolls
-
-
-class Roll:
-    """Thiss class represents a simple dice roll."""
-
-    # rolls the dice and calculates total on init
-    def __init__(self, dice: int, sides: int) -> None:
-        self.exp = f'{dice}d{sides}'
-        self.rolls = self.__get_rolls(dice, sides)
-        self.total = self.__get_total(self.rolls)
-
-    # returns the object represented as a string
-    def __repr__(self) -> str:
-        return f'{self.exp} : {self.rolls}'
-
-    # returns a list of dice results from a single roll
-    @staticmethod
-    def __get_rolls(dice: int, sides: int) -> list[int]:
-        rolls = []
-        for i in range(dice):
-            rolls.append(randint(1, sides))
-        return rolls
+    def roll_dice(n: int, d: int) -> Roll:
+        return Roll(n, d)
     
-    # returns the total sum of a set of dice rolls
     @staticmethod
-    def __get_total(rolls: list[int]) -> int:
-        total = 0
-        for roll in rolls:
-            total += roll
-        return total
+    def keep_high(r: Roll, n: int) -> int:
+        result = 0
+        rolls = sorted(r.rolls, reverse=True)
+        for i in range(min(len(rolls), n)):
+            result += rolls[i]
+        return result
+
+    @staticmethod
+    def keep_low(r: Roll, n: int) -> int:
+        result = 0
+        rolls = sorted(r.rolls, reverse=False)
+        for i in range(min(len(rolls), n)):
+            result += rolls[i]
+        return result
+
+    @staticmethod
+    def count_success(r: Roll, operator: str, target: int) -> int:
+        result = 0
+        if operator == '=':
+            result = r.rolls.count(target)
+        elif operator == '>':
+            for x in r.rolls:
+                if x > target:
+                    result += 1
+        elif operator == '>=':
+            for x in r.rolls:
+                if x >= target:
+                    result += 1
+        elif operator == '<':
+            for x in r.rolls:
+                if x < target:
+                    result += 1
+        elif operator == '<=':
+            for x in r.rolls:
+                if x <= target:
+                    result += 1
+        return result
